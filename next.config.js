5522f7409e2d0790d096895cbdd0f6b9554c0a16
odeteassistenteoliveira <odete.assistente@gmail.com>
2026-03-26 07:44:56 -0300

    fix: remove allowedOrigins restriction from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-    serverActions: {
-      allowedOrigins: ["localhost:3000"],
-        },
-    },
   images: {
-    remotePatterns: [
-{
-        protocol: "https",
-                  hostname: "*.supabase.co",
-          },
-          ],
-      },
-      };
-
+    remotePatterns: [{ protocol: "https", hostname: "*.supabase.co" }],
+  },
+};
 module.exports = nextConfig;
